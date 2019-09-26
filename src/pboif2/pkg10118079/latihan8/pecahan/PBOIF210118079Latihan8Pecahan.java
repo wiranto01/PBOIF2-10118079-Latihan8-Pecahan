1f33d73c92834862a5a5ad591ef0ba03ef6d6025
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:24:40 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan8/pecahan/PBOIF210118079Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
